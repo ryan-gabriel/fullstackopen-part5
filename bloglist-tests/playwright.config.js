d2ce9d6cfa013c597fcda297a4c6785b07ea01aa
ryan <ryangs112233@gmail.com>
2025-04-29 13:17:04 +0700

    Exercise 5.21: Blog List End To End Testing, step 5 & Exercise 5.22: Blog List End To End Testing, step 6

--- bloglist-tests/playwright.config.js
+++ bloglist-tests/playwright.config.js
@@ -78,6 +78,6 @@ export default defineConfig({
   //   url: 'http://127.0.0.1:3000',
   //   reuseExistingServer: !process.env.CI,
   // },
-  timeout:3000
+  timeout: 3000
 });
 
--- bloglist-tests/tests/blogListApp.spec.js
+++ bloglist-tests/tests/blogListApp.spec.js
@@ -15,11 +15,8 @@ describe("Blog app", () => {
   });
 
   test("Login form is shown", async ({ page }) => {
-    const loginInput = await page.getByTestId("username");
-    await expect(loginInput).toBeVisible();
-
-    const passwordInput = await page.getByTestId("password");
-    await expect(passwordInput).toBeVisible();
+    await expect(page.getByTestId("username")).toBeVisible();
+    await expect(page.getByTestId("password")).toBeVisible();
   });
 
   describe("Login", () => {
@@ -29,9 +26,7 @@ describe("Blog app", () => {
       await page.getByRole("button", { name: "login" }).click();
 
       await expect(page.getByText("John Doe logged in")).toBeVisible();
-      await expect(
-        page.getByText("Wrong username or password")
-      ).not.toBeVisible();
+      await expect(page.getByText("Wrong username or password")).not.toBeVisible();
     });
 
     test("fails with wrong credentials", async ({ page }) => {
@@ -49,10 +44,11 @@ describe("Blog app", () => {
       await page.getByTestId("username").fill("john");
       await page.getByTestId("password").fill("12345678");
       await page.getByRole("button", { name: "login" }).click();
+      await expect(page.getByText("John Doe logged in")).toBeVisible();
     });
 
     test("a new blog can be created", async ({ page }) => {
-      await page.getByRole("button", { name: "new note" }).click();
+      await page.getByRole("button", { name: "new blog" }).click();
       await page.getByTestId("title").fill("Testing blog title");
       await page.getByTestId("author").fill("John Doe");
       await page.getByTestId("url").fill("http://example.com");
@@ -63,17 +59,18 @@ describe("Blog app", () => {
 
     describe("and a blog exists", () => {
       beforeEach(async ({ page }) => {
-        await page.getByRole("button", { name: "new note" }).click();
+        await page.getByRole("button", { name: "new blog" }).click();
         await page.getByTestId("title").fill("Testing blog title");
         await page.getByTestId("author").fill("John Doe");
         await page.getByTestId("url").fill("http://example.com");
 
         await page.getByRole("button", { name: "Create" }).click();
+        await expect(page.getByText("Testing blog title John Doe")).toBeVisible();
       });
 
       test("it can be liked", async ({ page }) => {
         const button = page.getByRole("button", { name: "view" });
-        expect(button).toBeVisible();
+        await expect(button).toBeVisible();
         await button.click();
 
         const blogDetail = page.getByText("Testing blog title John Doe");
@@ -81,10 +78,57 @@ describe("Blog app", () => {
         await expect(div).toContainText("likes 0");
 
         const likeButton = page.getByRole("button", { name: "like" });
-        expect(likeButton).toBeVisible();
+        await expect(likeButton).toBeVisible();
         await likeButton.click();
 
         await expect(div).toContainText("likes 1");
+      });
+
+      test("it can be deleted by the user who created it", async ({ page }) => {
+        const button = page.getByRole("button", { name: "view" });
+        await expect(button).toBeVisible();
+        await button.click();
+
+        const blogDetail = page.getByText("Testing blog title John Doe");
+        const div = blogDetail.locator("..").locator(".blogDetail");
+        await expect(div).toContainText("likes");
+        page.once("dialog", async (dialog) => {
+          console.log(dialog.message());
+          await dialog.accept();
+        });
+        const deleteButton = page.getByRole("button", { name: "remove" });
+        await expect(deleteButton).toBeVisible();
+        await deleteButton.click();
+        await expect(page.getByText("Testing blog title John Doe")).not.toBeVisible();
+      });
+
+      describe("and another user is logged in", () => {
+        beforeEach(async ({ page, request }) => {
+          await page.getByRole("button", { name: "Log Out" }).click();
+          await expect(page.getByText("John Doe logged in")).not.toBeVisible();
+
+          await request.post("/api/users", {
+            data: {
+              name: "Jane Doe",
+              username: "jane",
+              password: "12345678",
+            },
+          });
+
+          await page.getByTestId("username").fill("jane");
+          await page.getByTestId("password").fill("12345678");
+          await page.getByRole("button", { name: "login" }).click();
+          await expect(page.getByText("Jane Doe logged in")).toBeVisible();
+        });
+
+        test("it cannot be deleted by another user", async ({ page }) => {
+          const button = page.getByRole("button", { name: "view" });
+          await expect(button).toBeVisible();
+          await button.click();
+
+          const deleteButton = page.getByRole("button", { name: "remove" });
+          await expect(deleteButton).not.toBeVisible();
+        });
       });
     });
   });
